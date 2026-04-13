cb4e517e359480ef04234b8d516c19b42d9fd47c
pnnprn <131662999+ppanturat@users.noreply.github.com>
2026-04-13 06:15:55 +0000

    add test functions

--- test_subtest_age.py
+++ test_subtest_age.py
@@ -2,6 +2,20 @@ import unittest
 from age import categorize_by_age
 
 class TestIsGolden(unittest.TestCase):
+    def test_child_age(self):
+        for age in range(0, 10):
+            with self.subTest(age=age):
+                result = categorize_by_age(age)
+                print(f"{age} is considered as a child.")
+                self.assertEqual(result, "Child")
+
+    def test_adolescent_age(self):
+        for age in range(10, 19):
+            with self.subTest(age=age):
+                result = categorize_by_age(age)
+                print(f"{age} is considered as an adolescent.")
+                self.assertEqual(result, "Adolescent")
+
     def test_golden_age(self):
         for age in range(66, 151):
             with self.subTest(age=age):
